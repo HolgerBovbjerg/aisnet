dfb95ba9a91441be12ef3a2cbf244b2cc7665269
Holger Severin Bovbjerg <holger.bovbjerg@hotmail.com>
2024-11-12 13:20:12 -0500

    Refactor utils functions

--- source/augment/utils.py
+++ source/augment/utils.py
@@ -17,9 +17,17 @@ def get_augmentor(name, **kwargs):
     raise ValueError(f"Unknown augmentor name: {name}")
 
 
+class ComposedAugmentorWrapper:
+    def __init__(self, composed_augmentors):
+        self.composed_augmentors = composed_augmentors
+
+    def __call__(self, sample):
+        return self.composed_augmentors(sample).samples
+
+
 def get_composed_augmentations(config, sampling_rate: int = 16000):
     augmentations = [
         get_augmentor(key, sampling_rate=sampling_rate, **config[key])
         for key in config.keys()
     ]
-    return Compose(augmentations)
+    return ComposedAugmentorWrapper(Compose(augmentations, output_type="dict"))
