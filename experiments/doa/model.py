15414d1db33b5f9c5cae217397f0f4206b46f62f
Holger Severin Bovbjerg <holger.bovbjerg@hotmail.com>
2024-12-03 15:48:04 -0500

    Support for pretrained SSL model via S3PRL

--- experiments/doa/model.py
+++ experiments/doa/model.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from logging import getLogger
 from typing import Tuple, Union, List, Optional
 from math import ceil
@@ -10,6 +10,7 @@ from torch import nn
 from source.utils import count_parameters
 from source.nnet.feature_extraction import build_feature_extractor, FeatureExtractorConfig
 from source.nnet.encoder import build_encoder, EncoderConfig
+from source.models.pretrained import S3PRLFeaturizer, S3PRLFeaturizerConfig
 from source.models.utils import load_partial_checkpoints
 from source.nnet.modules.input_projection import InputProjection
 
@@ -23,6 +24,8 @@ def list_to_tensor(input_list):
 
 @dataclass
 class DoaModelConfig:
+    feature_extractor: Union[FeatureExtractorConfig, dict]
+    encoder: Union[EncoderConfig, dict]
     microphone_array: Tuple[List[float], ...] = ([0, -0.09, 0], [0, 0.09, 0])
     elevation_resolution: float = 10.
     azimuth_resolution: float = 5.
@@ -34,8 +37,6 @@ class DoaModelConfig:
     feature_dropout: float = 0.
     feature_dropout_first: bool = False
     encoder_embedding_dim: int = 512
-    feature_extractor: Optional[Union[FeatureExtractorConfig, dict]] = None
-    encoder: Optional[Union[EncoderConfig, dict]] = None
     sample_rate: int = 16000
     num_channels: int = 2
 
@@ -114,10 +115,100 @@ class DoaModel(nn.Module):
         return x, lengths
 
 
+@dataclass
+class DoaPretrainedModelConfig:
+    feature_extractor: Union[S3PRLFeaturizerConfig, dict]
+    microphone_array: Tuple[List[float], ...] = ([0, -0.09, 0], [0, 0.09, 0])
+    elevation_resolution: float = 10.
+    azimuth_resolution: float = 5.
+    elevation_range: Tuple[float, float] = (90., 90.)
+    azimuth_range: Tuple[float, float] = (-90., 90.)
+    feature_dim: int = 768
+    embedding_dim: int = 256
+    feature_channels: int = 2
+    feature_projection: bool = True
+    feature_dropout: float = 0.
+    feature_dropout_first: bool = False
+    sample_rate: int = 16000
+    num_channels: int = 2
+
+    def __post_init__(self):
+        if isinstance(self.feature_extractor, (dict, DictConfig)):
+            self.feature_extractor = S3PRLFeaturizerConfig(**self.feature_extractor.config)
+        elif not isinstance(self.feature_extractor, S3PRLFeaturizerConfig):
+            raise ValueError("Wrong input for 'feature_extractor'.")
+
+
+class DoaPretrainedModel(nn.Module):
+    """
+    Pretrained DoA model main module.
+    """
+
+    def __init__(self,
+                 cfg: DoaPretrainedModelConfig):
+        super().__init__()
+
+        self.azimuth_resolution = cfg.azimuth_resolution
+        self.elevation_resolution = cfg.elevation_resolution
+        self.elevation_range = cfg.elevation_range
+        self.azimuth_range = cfg.azimuth_range
+        self.num_channels = cfg.num_channels
+        self.microphone_array = list_to_tensor(cfg.microphone_array)
+        self.n_mics = len(self.microphone_array)
+        assert self.n_mics == self.num_channels, "Microphone array elements and channels should be the same"
+        self.feature_dim = cfg.feature_dim
+        self.feature_channels = cfg.feature_channels
+        self.sample_rate = cfg.sample_rate
+        self.feature_extractor = S3PRLFeaturizer(**asdict(cfg.feature_extractor))
+        self.feature_projection = InputProjection(self.feature_dim * self.feature_channels, cfg.embedding_dim, dropout_rate=cfg.feature_dropout, dropout_first=cfg.feature_dropout_first) \
+            if cfg.feature_projection else nn.Identity()
+        self.elevation_angles, self.azimuth_angles = self._get_elevation_and_azimuth_angles()
+        self.classifier = nn.Linear(cfg.embedding_dim, len(self.elevation_angles) * len(self.azimuth_angles))
+
+    def _get_elevation_and_azimuth_angles(self):
+        elevation_span = abs(self.elevation_range[1] - self.elevation_range[0])
+        num_directions_elevation = ceil(elevation_span / self.elevation_resolution) if elevation_span else 1
+        if elevation_span:
+            elevation_angles = torch.arange(num_directions_elevation) * self.elevation_resolution + \
+                               self.elevation_range[0]
+        else:
+            elevation_angles = torch.tensor([self.elevation_range[0]])
+        azimuth_span = abs(self.azimuth_range[1] - self.azimuth_range[0])
+        num_directions_azimuth = ceil(azimuth_span / self.azimuth_resolution) if azimuth_span else 1
+        if azimuth_span:
+            azimuth_angles = torch.arange(num_directions_azimuth) * self.azimuth_resolution + self.azimuth_range[0]
+        else:
+            azimuth_angles = torch.tensor([self.azimuth_range[0]])
+
+        return elevation_angles, azimuth_angles
+
+    def forward(self, x, lengths=None):
+        # Extract pretrained model features
+        features_out = []
+        feature_lengths = None
+        for i in range(self.num_channels):
+            features, feature_lengths = self.feature_extractor(x[:, i], lengths)
+            features_out.append(features)
+        # Stack channels
+        x = torch.cat(features_out, dim=-1)
+        lengths = feature_lengths
+
+        # Project features
+        x = self.feature_projection(x)
+
+        # Classify from pretrained model features
+        x = self.classifier(x)
+        return x, lengths
+
+
 def build_model(config):
     logger.info(f"Model Config: {OmegaConf.to_object(config.model)}")
-    model_config = DoaModelConfig(**config.model)
-    model = DoaModel(model_config)
+    if config.model.feature_extractor.name == "pretrained":
+        model_config = DoaPretrainedModelConfig(**config.model)
+        model = DoaPretrainedModel(model_config)
+    else:
+        model_config = DoaModelConfig(**config.model)
+        model = DoaModel(model_config)
     logger.info(f"Created model with {count_parameters(model)} parameters.")
     # Load partial checkpoints if specified in the config
     partial_checkpoints = config.model.get("partial_checkpoints", {})
---
+++ source/models/pretrained/pretrained.py
@@ -0,0 +1,117 @@
+from typing import List, Optional, Dict, Union
+from dataclasses import dataclass, asdict
+
+import torch
+import torch.nn as nn
+from s3prl.nn import S3PRLUpstream, Featurizer
+
+from source.models.utils import freeze_model_parameters
+
+
+def get_s3prl_upstream_model(model_name: str, freeze_model: bool = True, extra_config: Optional[dict] = None) -> S3PRLUpstream:
+    model = S3PRLUpstream(model_name, extra_conf=extra_config)
+    if freeze_model:
+        freeze_model_parameters(model)
+    return model
+
+
+def get_s3prl_featurizer(model: S3PRLUpstream, layer_selection: Optional[List[int]] = None, normalizer: bool = True) -> nn.Module:
+    featurizer = Featurizer(model, layer_selections=layer_selection, normalize=normalizer)
+    return featurizer
+
+
+@dataclass
+class S3PRLFeaturizerConfig:
+    model_name: str  # Name of the S3PRL upstream model
+    freeze_model: bool = True  # Whether to freeze upstream model parameters
+    extra_config: Optional[Dict] = None  # Additional configuration for the upstream model
+    layer_selection: Optional[List[int]] = None  # Layers to use for feature extraction
+    normalizer: bool = True  # Whether to normalize the extracted features
+
+
+class S3PRLFeaturizer(nn.Module):
+    def __init__(
+        self,
+        model_name: str,
+        freeze_model: bool = True,
+        extra_config: Optional[Dict] = None,
+        layer_selection: Optional[List[int]] = None,
+        normalizer: bool = True
+    ):
+        """
+        Wrapper module to combine an S3PRL upstream model and a featurizer.
+
+        Args:
+            model_name (str): Name of the S3PRL upstream model.
+            freeze_model (bool): Whether to freeze the upstream model parameters.
+            extra_config (dict, optional): Additional configuration for the upstream model.
+            layer_selection (list of int, optional): Layers to use for feature extraction.
+            normalizer (bool): Whether to normalize the extracted features.
+        """
+        super().__init__()
+        self.freeze_model = freeze_model
+        self.model = get_s3prl_upstream_model(
+            model_name=model_name,
+            freeze_model=freeze_model,
+            extra_config=extra_config
+        )
+        if self.freeze_model:
+           self.model.eval()
+        self.featurizer = get_s3prl_featurizer(
+            model=self.model,
+            layer_selection=layer_selection,
+            normalizer=normalizer
+        )
+
+    def forward(self, wavs: torch.Tensor, wavs_len: torch.Tensor):
+        """
+        Forward pass through the upstream model and featurizer.
+
+        Args:
+            wavs (torch.Tensor): Input waveforms, shape (batch_size, max_length).
+            wavs_len (torch.Tensor): Lengths of each waveform in the batch.
+
+        Returns:
+            torch.Tensor, torch.Tensor: Processed features and corresponding lengths.
+        """
+        with torch.set_grad_enabled(not self.freeze_model):
+            if self.freeze_model:
+                self.model.eval()
+            all_hs, all_hs_len = self.model(wavs, wavs_len)
+        hs, hs_len = self.featurizer(all_hs, all_hs_len)
+        return hs, hs_len
+
+
+class PretrainedModel(nn.Module):
+    def __init__(self, source: str, config: Union[S3PRLFeaturizerConfig, Dict]):
+        """
+        A general wrapper to select and initialize a pretrained model wrapper based on the source.
+
+        Args:
+            source (str): The source of the pretrained model ("s3prl" or "local").
+            config (Union[UpstreamFeaturizerWrapperConfig, Dict]): Configuration for the selected model wrapper.
+        """
+        super().__init__()
+
+        if source == "s3prl":
+            if isinstance(config, S3PRLFeaturizerConfig):
+                self.wrapper = S3PRLFeaturizer(**asdict(config))
+            else:
+                self.wrapper = S3PRLFeaturizer(**config)
+        elif source == "local":
+            raise ValueError(f"Source: {source} is not yet implemented. Will be available in the future")
+        else:
+            raise ValueError(f"Unsupported source: {source}. Valid options are 's3prl' and 'local'.")
+
+    def forward(self, wavs: torch.Tensor, wavs_len: torch.Tensor):
+        """
+        Forward pass through the selected featurizer wrapper.
+
+        Args:
+            wavs (torch.Tensor): Input waveforms, shape (batch_size, max_length).
+            wavs_len (torch.Tensor): Lengths of each waveform in the batch.
+
+        Returns:
+            torch.Tensor, torch.Tensor: Processed features and corresponding lengths.
+        """
+        return self.wrapper(wavs, wavs_len)
